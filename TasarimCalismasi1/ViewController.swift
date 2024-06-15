140eafc6c1397ae3b839bb0f6f26843c1d39d415
beratince19 <inceberat626@gmail.com>
2024-06-15 22:15:23 +0300

    Initial Commit

---
+++ TasarimCalismasi1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasarimCalismasi1
+//
+//  Created by Berat Tugay İnce on 15.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
